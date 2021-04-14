007b3856a64b591b8d55ced938d1c94636a6ea63
Pablo Brizzi <brizzipablo@gmail.com>
2021-04-14 12:50:21 -0300

    modificacion de tipo de carga de datos

--- TP1_p3/Auto_APP.py
+++ TP1_p3/Auto_APP.py
@@ -2,6 +2,7 @@ from TP1_p3.TAD_auto import *
 
 '''
 TAD_Auto = {
+    pat: string,
     km: float,
     marca: string,
     mod: string,
@@ -15,8 +16,11 @@ TAD_Auto = {
 crearAuto() 
 #Crea un auto vacío
 
-cargarAuto(auto, km, marca, mod, prec)
+cargarAuto(auto, pat, km, marca, mod, prec)
 #Agrega un Auto nuevo
+
+verPat(auto)
+#Retorna la Patente del Auto
 
 verKm(auto)
 #Retorna los Kms del Auto
@@ -51,14 +55,15 @@ auto1 = crearAuto()
 print("____"*3, "\n\r")
 print("Datos Auto 1\n\r")
 
+pat1= input("Ingrese la Patente: \n\r")
 km1 = float(input("Ingrese los km: \n\r"))
 marc1 = input("Ingrese la marca: \n\r")
 mod1 = input("Ingrese el modelo: \n\r")
 prec1 = float(input("Ingrese el precio: \n\r"))
 
-cargarAuto(auto1, km1, marc1, mod1, prec1)
+cargarAuto(auto1, pat1, km1, marc1, mod1, prec1)
 
-print(verKm(auto1), " | ", verMarca(auto1), " | ", verModelo(auto1), " | ", verPrecio(auto1), "\n\r")
+print(verPat(auto1), " | ",verKm(auto1), " | ", verMarca(auto1), " | ", verModelo(auto1), " | ", verPrecio(auto1), "\n\r")
 
 #Creo el segundo auto y agrego sus datos
 auto2 = crearAuto()
@@ -66,15 +71,16 @@ auto2 = crearAuto()
 print("____"*3, "\n\r")
 print("Datos Auto 2\n\r")
 
+pat2= input("Ingrese la Patente: \n\r")
 km2 = float(input("Ingrese los km: \n\r"))
 marc2 = input("Ingrese la marca: \n\r")
 mod2 = input("Ingrese el modelo: \n\r")
 prec2 = float(input("Ingrese el precio: \n\r"))
 
-cargarAuto(auto2, km2, marc2, mod2, prec2)
+cargarAuto(auto2, pat2, km2, marc2, mod2, prec2)
 
 #Comparo cuál es el de menor precio e imprimo sus datos
 if(verPrecio(auto1)<verPrecio(auto2)):
-    print(verKm(auto1), " | ", verMarca(auto1), " | ", verModelo(auto1), " | ", verPrecio(auto1))
+    print(verPat(auto1), " | ", verKm(auto1), " | ", verMarca(auto1), " | ", verModelo(auto1), " | ", verPrecio(auto1))
 else:
-    print(verKm(auto2), " | ", verMarca(auto2), " | ", verModelo(auto2), " | ", verPrecio(auto2))
+    print(verPat(auto2), " | ", verKm(auto2), " | ", verMarca(auto2), " | ", verModelo(auto2), " | ", verPrecio(auto2))
--- TP1_p3/TAD_auto.py
+++ TP1_p3/TAD_auto.py
@@ -2,45 +2,62 @@
 
 def crearAuto():
     #Crea un auto vacío
-    auto=[]
+    auto=["", 0, "", "", 0]
     return auto
 
-def cargarAuto(auto, km, marca, mod, prec):
+def cargarAuto(auto, pat ,km, marca, mod, prec):
     #Agrega un Auto nuevo
+    auto[0]=pat
+    auto[1]=km
+    auto[2]=marca
+    auto[3]=mod
+    auto[4]=prec
+
+''' OTRA MANERA DE HACERLO   
+    auto.append(pat)
     auto.append(km)
     auto.append(marca)
     auto.append(mod)
-    auto.append(prec)
+    auto.append(prec)'''
 
-
-def verKm(auto):
+def verPat(auto):
     return auto[0]
     #Retorna los Kms del Auto
 
-def verMarca(auto):
+
+def verKm(auto):
     return auto[1]
+    #Retorna los Kms del Auto
+
+def verMarca(auto):
+    return auto[2]
     #Retorna la Marca del auto
 
 def verModelo(auto):
-    return auto[2]
+    return auto[3]
     #Retorna el modelo del Auto
 
 def verPrecio(auto):
-    return auto[3]
+    return auto[4]
     #Retorna el Precio del Auto
 
+def modKm(auto, otraPat):
+    auto[0]=otraPat
+    #Modifica el Km del Auto
+
+
 def modKm(auto, otroKm):
-    auto[0]=otroKm
+    auto[1]=otroKm
     #Modifica el Km del Auto
 
 def modMarca(auto, otraMarca):
-    auto[1]=otraMarca
+    auto[2]=otraMarca
     #Modifica la Marca del Auto
 
 def modModelo(auto, otroMod):
-    auto[2]=otroMod
+    auto[3]=otroMod
     #Modifica el modelo del Auto
 
 def modPrecio(auto, otroPrec):
-    auto[3]=otroPrec
+    auto[4]=otroPrec
     #Modifica el precio del Auto
